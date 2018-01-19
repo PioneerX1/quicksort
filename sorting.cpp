dece3c8ce1a0c985cb0aaa4f3aec9735a0fe6346
Mick Sexton <mick.sexton@fulcrum.net>
2018-01-19 15:04:48 -0800

    add quicker sort for faster partitioning

--- sorting.cpp
+++ sorting.cpp
@@ -5,30 +5,32 @@
 using std::vector;
 using std::swap;
 
-int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
-    }
-  }
-  swap(a[l], a[j]);
-  return j;
-}
+void quickerSort(vector<int> &a, int left, int right) {
+      int i = left, j = right;
+      int tmp;
+      int pivot = a[(left + right) / 2];
 
-void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
-    return;
-  }
+      // partitioning the vector
+      while (i <= j) {
+            while (a[i] < pivot)
+                  i++;
+            while (a[j] > pivot)
+                  j--;
+            if (i <= j) {
+                  tmp = a[i];
+                  a[i] = a[j];
+                  a[j] = tmp;
+                  i++;
+                  j--;
+            }
+      };
 
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+      // recursive calls
+      if (left < j)
+            quickerSort(a, left, j);
+      if (i < right)
+            quickerSort(a, i, right);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
 }
 
 int main() {
@@ -38,7 +40,7 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  randomized_quick_sort(a, 0, a.size() - 1);
+  quickerSort(a, 0, a.size() - 1);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
